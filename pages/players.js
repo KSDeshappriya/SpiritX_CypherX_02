a609ce269dce3bfc3fd6c0f4fb74e462193936eb
Chamini Maduwanthi <chaminimaduwanthi97@gmail.com>
2025-03-09 18:54:23 +0530

    Create players.js

---
+++ pages/players.js
@@ -0,0 +1,11 @@
+import Header from '../components/Header';
+import Players from '../components/Players';
+
+export default function PlayersPage() {
+  return (
+    <>
+      <Header />
+      <Players />
+    </>
+  );
+}
