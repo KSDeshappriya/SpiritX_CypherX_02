fa3bc1af0c3f1ea78dbc4907d8d24e8575ecb2ca
ishanirathnayaka <ishanirathnayaka00@gmail.com>
2025-03-09 15:47:41 +0530

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;800&display=swap' rel='stylesheet' />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
